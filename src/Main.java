601942732e908bf6e1b74aea02e3f8af62607133
hellowoori <myahn0607@gmail.com>
2024-02-06 23:15:58 +0900

    1. 문자 찾기

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        IQuest q = new Quest2();
+        q.run();
+    }
+}
---
+++ src/Quest1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+// 1. 문자 찾기
+public class Quest1 implements IQuest{
+
+    @Override
+    public void run() {
+        Quest1 q = new Quest1();
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        char c = scanner.next().charAt(0);
+        System.out.print(q.solution(str, c));
+    }
+
+    public int solution(String str, char targetC) {
+        final String newStr = str.toUpperCase();
+        final char newTargetC = Character.toUpperCase(targetC);
+
+        // 해법 1)
+        // int answer = 0;
+        // for (int i = 0; i < newStr.length(); i++) {
+        //    if (newStr.charAt(i) == newTargetC) answer++;
+        // }
+        // return answer;
+
+        // 해법 2)
+        // int answer = 0;
+        // for (char x : newStr.toCharArray()) {
+        //    if (x == newTargetC) {
+        //        answer++;
+        //    }
+        // }
+        // return answer;
+
+        // 해법 3)
+        return (int) newStr.chars()
+                .mapToObj(c -> (char) c)
+                .filter(c -> c == newTargetC)
+                .count();
+    }
+}
